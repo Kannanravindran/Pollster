1672c06c337b1d333439452d924728b4a1519915
Kannan Ravindran <kannan.cyberpunk@gmail.com>
2019-12-14 16:46:07 +0530

    Fixing statistics page

--- src/components/subComponents/StatsAdminPanel.jsx
+++ src/components/subComponents/StatsAdminPanel.jsx
@@ -12,54 +12,80 @@ var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 var config = require("../../config.json");
 
 class StatsAdminPanel extends Component {
-  //   _isMounted = false;
+  _isMounted = false;
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { data: {} };
   }
 
+  storeAnswers = surveyId => {
+    axios
+      .get(config.baseurl + "stats/", {
+        params: {
+          uid: this.props.uid
+        }
+      })
+      .then(res => {
+        var data = res.data.stat;
+        this.setState({ data });
+        // console.log(this.state);
+      });
+  };
+
+  componentDidMount = () => {
+    this._isMounted = true;
+    this.props.adminPrivileges.forEach(survey => {
+      this.storeAnswers(survey);
+    });
+  };
+  componentWillUnmount = () => {
+    this._isMounted = false;
+  };
   activateTab = e => {
     return !this.props.adminPrivileges.includes(e);
   };
 
-  getBarChart = surveyId => {
-    const options = {
-      animationEnabled: true,
-      exportEnabled: false,
-      theme: "light2", //"light1", "dark1", "dark2"
-      title: {
-        text: "Simple Column Chart with Index Labels"
-      },
-      data: [
-        {
-          type: "column", //change type to bar, line, area, pie, etc
-          //indexLabel: "{y}", //Shows y value on all Data Points
-          indexLabelFontColor: "#5A5757",
-          indexLabelPlacement: "outside",
-          dataPoints: [
-            { x: 10, y: 71 },
-            { x: 20, y: 55 },
-            { x: 30, y: 50 },
-            { x: 40, y: 65 },
-            { x: 50, y: 71 },
-            { x: 60, y: 68 },
-            { x: 70, y: 38 },
-            { x: 80, y: 92, indexLabel: "Highest" },
-            { x: 90, y: 54 },
-            { x: 100, y: 60 },
-            { x: 110, y: 21 },
-            { x: 120, y: 49 },
-            { x: 130, y: 36 }
-          ]
-        }
-      ]
-    };
-
-    return <CanvasJSChart options={options} />;
+  getStateData = (n, r) => {
+    if (this.state.data[n] === undefined) {
+      return 0;
+    } else {
+      return this.state.data[n][r];
+    }
   };
 
+  getStatistics = surveyId => {
+    console.log("table");
+    if (this.state.data[surveyId] !== undefined) {
+      console.log("table");
+      return (
+        <Table hover>
+          <thead>
+            <tr>
+              <th>Property</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td># Users</td>
+              <td>{this.state.data[surveyId]["user_count"]}</td>
+            </tr>
+            <tr>
+              <td>Average</td>
+              <td>{this.state.data[surveyId]["avg"]}</td>
+            </tr>
+            <tr>
+              <td>Median</td>
+              <td>{this.state.data[surveyId]["median"]}</td>
+            </tr>
+          </tbody>
+        </Table>
+      );
+    }
+  };
   getDonutChart = surveyId => {
+    console.log("state: ", this.state);
     const options = {
       animationEnabled: true,
       title: {
@@ -80,8 +106,8 @@ class StatsAdminPanel extends Component {
           indexLabel: "{name}: {y}",
           yValueFormatString: "#,###'%'",
           dataPoints: [
-            { name: "Like", y: 60 },
-            { name: "Dislike", y: 40 }
+            { name: "Like", y: this.getStateData(surveyId, "Y") },
+            { name: "Dislike", y: this.getStateData(surveyId, "N") }
           ]
         }
       ]
@@ -99,16 +125,16 @@ class StatsAdminPanel extends Component {
         <TabPanel>
           <Container>
             <Row>
+              <Col>{this.getStatistics("1")}</Col>
               <Col>{this.getDonutChart("1")}</Col>
-              <Col>{this.getBarChart("1")}</Col>
             </Row>
           </Container>
         </TabPanel>
         <TabPanel>
           <Container>
             <Row>
+              <Col>{this.getStatistics("2")}</Col>
               <Col>{this.getDonutChart("2")}</Col>
-              <Col>{this.getBarChart("2")}</Col>
             </Row>
           </Container>
         </TabPanel>
